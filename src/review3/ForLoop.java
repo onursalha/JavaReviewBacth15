dd57bf09a9854227df193a8f8403cade4121b62a
onursalha <onursalha22@gmail.com>
2023-01-12 21:04:11 -0600

    end of review class

---
+++ src/review3/ForLoop.java
@@ -0,0 +1,37 @@
+package review3;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=5; i++) {
+            System.out.println("Hello "+i);
+
+        }
+
+        System.out.println("--------------------");
+
+        for (int a=0;  a<8; a+=4){
+            System.out.println("Bye");
+
+        }
+
+
+
+        for (int i=10; i>=5; i--){
+            System.out.println(i);
+
+        }
+        for(int i=5; i<=1; i-- ) {
+            System.out.println("Happy");
+        }
+
+        for (int i=1; i<=6; i++){
+            if (i%2==0){
+
+
+            System.out.println(" even number "+i);
+            }
+        }
+
+    }
+}
---
+++ src/review3/SwitchDemo2.java
@@ -0,0 +1,7 @@
+package review3;
+
+public class SwitchDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
